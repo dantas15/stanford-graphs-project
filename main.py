52da1aa56f5d80255d4e82859d85c1f00d7b5041
Gustavo Dantas Galote <gustav0d@protonmail.com>
2023-12-15 02:33:13 -0300

    ✨ (visualization): visualize top hubs and authorities

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 from read_graph import read_graph_from_file
+from visualize_top_hubs_and_authorities import visualize_hubs_and_authorities
 
 if __name__ == "__main__":
     graph_file_path = sys.argv[1];
     graph = read_graph_from_file(graph_file_path)
-    print(f"Number of nodes: {graph.number_of_nodes()}")
-    print(f"Number of edges: {graph.number_of_edges()}")
+    visualize_hubs_and_authorities(graph)
